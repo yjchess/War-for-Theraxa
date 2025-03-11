bbd1ea7d8a6bfc6e1153db53b84d3659f986a847
Yang-Jian Zhou <yangjian.zhou@live.co.uk>
2025-03-11 19:22:39 +0000

    refactor: added a registry for transitioning between levels

--- campaign/campaign_map/campaign_map.gd
+++ campaign/campaign_map/campaign_map.gd
@@ -1,23 +1,23 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+const REGISTRY = {
+	castle = "res://campaign/campaign_map/castle/castle.tscn",
+	level_one = "res://levels/one.tcsn",
+	level_two = "res://levels/two.tcsn"
+}
 
 func _on_level_change(level):
 	var level_path = "res://levels/"+level+".tscn"
-	get_tree().change_scene_to_file(level_path)
-	pass # Replace with function body.
-
+	if validate_scene(level_path):
+		get_tree().change_scene_to_file(level_path)
+	
 
 func _on_castle_button_pressed():
-	print("CASTLE BUTTON PRESSED")
-	get_tree().change_scene_to_file("res://campaign/campaign_map/castle/castle.tscn")
-	pass # Replace with function body.
+	var castle_path = "res://campaign/campaign_map/castle/castle.tscn"
+	if validate_scene(castle_path):
+		get_tree().change_scene_to_file(castle_path)
+	
+func validate_scene(scene) -> bool:
+	if REGISTRY.has(scene): return true
+	else: return false
+	
