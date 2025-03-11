5ac19621f599c5fb5f5e10676cfd88d7cf902a79
Yang-Jian Zhou <yangjian.zhou@live.co.uk>
2025-03-11 20:51:02 +0000

    refactor: removed un-necessary loops

--- levels/ai/ai_handler.gd
+++ levels/ai/ai_handler.gd
@@ -8,14 +8,12 @@ func turn(computer_units, player_units, turns_played):
 			var calculated_move = unit.ai.calculate_best_movement_option(player_units)
 			if calculated_move != null:
 				unit.move(calculated_move[0], calculated_move[1])
-		
-		for unit:Unit in computer_units:
+
 			if unit.abilities_holder.get_child_count() > 0:
 				var calculated_ability = unit.ai.calculate_best_ability_options(player_units)
 				if calculated_ability != null:
 					unit.use_ability(calculated_ability[0], calculated_ability[1])
-				
-		for unit:Unit in computer_units:
+
 			var calculated_attack = unit.ai.calculate_best_attack_options()
 			if calculated_attack != null:
 				unit.attack(calculated_attack[0], calculated_attack[1])
